1709c2110bbee81fd50825a32598440b632e9430
Gabriel Almeida <ggalmeida1@users.noreply.github.com>
2021-07-03 19:02:10 -0300

    Fix encoded priv key

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f51ba1ae1faaed27da43"; }
+/******/ 		__webpack_require__.h = function() { return "023d9eed746e05a7d1d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
